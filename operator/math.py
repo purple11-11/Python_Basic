666372acc8063e466aa4ec0222032d475148f919
young <purple11_11@naver.com>
2024-02-04 17:30:01 +0900

    update: math 함수

--- operator/math.py
+++ operator/math.py
@@ -14,3 +14,11 @@ print(min(5, 12)) # 5
 # 반올림
 print(round(3.14)) # 3
 print(round(4.99)) #5
+
+from math import * # math 라이브러리 사용
+# 내림
+print(floor(4.99)) # 4
+# 올림
+print(ceil(3.14)) # 4
+# 제곱근
+print(sqrt(16)) # 4.0
